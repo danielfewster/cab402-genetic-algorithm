77854e2a58f53def3472adbde98bff4abcbc46bb
Daniel Fewster <34765204+danielfewster@users.noreply.github.com>
2021-04-23 23:06:52 +1000

    init commit

---
+++ CSharpSchedule/GeneticAlgorithm.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.FSharp.Core;
+
+namespace CAB402.CSharp
+{
+    public class GeneticAlgorithm
+    {
+        private class Individual // Class represents an Individual that has an array of Genes
+        {
+            private int[] genes;
+
+            public Individual(int[] genes) { this.genes = genes; }
+
+            public int[] Genes { get => genes; }
+        }
+
+        private class ScoredIndividual // Class represents a ScoredIndividual that has Genes, and a Fitness Score
+        {
+            public Tuple<int[], double> scoredIndividual;
+
+            public ScoredIndividual(int[] individual, double fitness)
+            {
+                scoredIndividual = new Tuple<int[], double>(individual, fitness);
+            }
+
+            public Individual Individual { get => new Individual(scoredIndividual.Item1); }
+
+            public double Fitness { get => scoredIndividual.Item2; }
+        }
+
+        private class Population // Class that represents a Population that holds an array of ScoredIndividuals
+        {
+            private ScoredIndividual[] scoredIndividuals;
+
+            public Population(ScoredIndividual[] scoredIndividuals) { this.scoredIndividuals = scoredIndividuals; }
+
+            public ScoredIndividual[] ScoredIndividuals
+            {
+                set => scoredIndividuals = value;
+                get => scoredIndividuals;
+            }
+        }
+
+        private static Random rand = new Random(); // Global random for performance
+
+        private static void SortDescending(ScoredIndividual[] si) // Helper Function 
+        {
+            ScoredIndividual temp;
+            for (int i = 0; i < si.Length; i++)
+                for (int j = i + 1; j < si.Length; j++)
+                    if (si[i].Fitness < si[j].Fitness)
+                    {
+                        temp = si[i];
+                        si[i] = si[j];
+                        si[j] = temp;
+                    }
+        }
+
+        private static ScoredIndividual Fitest(Population population)
+        {
+            var scoredIndividuals = population.ScoredIndividuals;
+            SortDescending(scoredIndividuals);
+            return scoredIndividuals[0];
+        }
+
+        private static Individual TournamentWinner(Population competitors)
+        {
+            return Fitest(competitors).Individual;
+        }
+
+        private static Individual TournamentSelect(Population population)
+        {
+            var n1 = population.ScoredIndividuals[rand.Next(population.ScoredIndividuals.Length)];
+            var n2 = population.ScoredIndividuals[rand.Next(population.ScoredIndividuals.Length)];
+            return TournamentWinner(new Population(new ScoredIndividual[] { n1, n2 }));
+        }
+
+        private static Individual CrossAt(Individual parent1, Individual parent2, int splitPoint)
+        {
+            // Get left + parent2 genes
+            var temp = new int[splitPoint + parent2.Genes.Length];
+            for (int i = 0; i < splitPoint; i++)
+                temp[i] = parent1.Genes[i];
+
+            for (int i = 0; i < parent2.Genes.Length; i++)
+                temp[i + splitPoint] = parent2.Genes[i];
+
+            // Make distinct
+            var crossover = new int[parent2.Genes.Length];
+            int idx = 0;
+            var alreadyIn = new Dictionary<int, bool>();
+            for (int i = 0; i < temp.Length; i++)
+                if (!alreadyIn.ContainsKey(temp[i]))
+                {
+                    crossover[idx++] = temp[i];
+                    alreadyIn.Add(temp[i], true);
+                }
+
+            return new Individual(crossover);
+        }
+
+        private static Individual Cross(Individual parent1, Individual parent2)
+        {
+            var index = rand.Next(1, parent1.Genes.Length - 1);
+            return CrossAt(parent1, parent2, index);
+        }
+
+        private static Individual ReverseMutateAt(Individual genes, int firstIndex, int secondIndex) 
+        {
+            var revGenes = genes.Genes;
+            Array.Reverse(revGenes);
+
+            var newGenes = new int[genes.Genes.Length];
+            for (int i = 0; i < newGenes.Length; i++)
+                if (i >= firstIndex && i <= secondIndex) // if in range of middle, get a reversed gene
+                    newGenes[i] = revGenes[i];
+                else
+                    newGenes[i] = genes.Genes[i]; // otherwise, get normal gene
+
+            return new Individual(newGenes);
+        }
+
+        private static Individual ReverseMutate(Individual chromosome) 
+        {
+            var first = rand.Next(chromosome.Genes.Length - 1);
+            var second = rand.Next(first + 1, chromosome.Genes.Length);
+            return ReverseMutateAt(chromosome, first, second);
+        }
+
+        private static double mutateProbability = 0.15;
+
+        private static bool GetProbability()
+        {
+            return rand.NextDouble() <= mutateProbability;
+        }
+
+        private static Individual PossiblyMutate(Individual genes) 
+        {
+            return GetProbability() ? ReverseMutate(genes) : genes;
+        }
+
+        private static Population ElitismSelection(Population parents, Population children) 
+        {
+            var scoredIndividuals = parents.ScoredIndividuals;
+            SortDescending(scoredIndividuals);
+
+            var newPopulation = new ScoredIndividual[children.ScoredIndividuals.Length + 10];
+            children.ScoredIndividuals.CopyTo(newPopulation, 0);
+
+            for (int i = 0; i < 10; i++)
+                newPopulation[i + children.ScoredIndividuals.Length] = scoredIndividuals[i];
+
+            return new Population(newPopulation);
+        }
+
+        private static ScoredIndividual Score(FSharpFunc<int[], double> fitnessFunction, Individual genes) 
+        {
+            return new ScoredIndividual(genes.Genes, fitnessFunction.Invoke(genes.Genes));
+        }
+
+        private static void Shuffle(int[] arr) // Fisher-Yates shuffle
+        {
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                int swapIndex = rand.Next(i + 1);
+                int tmp = arr[swapIndex];
+                arr[swapIndex] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+
+        private static int[] RandomPermutation(int n)
+        {
+            int[] genes = new int[n];
+            for (int i = 0; i < genes.Length; i++)
+                genes[i] = i;
+
+            Shuffle(genes);
+
+            return genes;
+        }
+
+        private static Population RandomIndividuals(FSharpFunc<int[], double> fitnessFunction, int numberGenes, int numberIndividuals) 
+        {
+            var generateIndividuals = new Individual[numberIndividuals]; // Generate Individuals
+            for (int i = 0; i < numberIndividuals; i++)
+                generateIndividuals[i] = new Individual(RandomPermutation(numberGenes));
+
+            var generateScoredIndividuals = new ScoredIndividual[numberIndividuals]; // Score Individuals
+            for (int i = 0; i < generateScoredIndividuals.Length; i++)
+                generateScoredIndividuals[i] = Score(fitnessFunction, generateIndividuals[i]);
+
+            return new Population(generateScoredIndividuals);
+        }
+
+        private static ScoredIndividual Procreate(FSharpFunc<int[], double> fitnessFunction, Population population) 
+        {
+            var p1 = TournamentSelect(population);
+            var p2 = TournamentSelect(population);
+            var crossedGenes = Cross(p1, p2);
+            return Score(fitnessFunction, PossiblyMutate(crossedGenes));
+        }
+
+        private static Population EvolveOneGeneration(FSharpFunc<int[], double> fitnessFunction, Population parentPopulation, int childPopulationLimit)
+        {
+            var createGeneration = new ScoredIndividual[childPopulationLimit];
+            for (int i = 0; i < childPopulationLimit; i++)
+                createGeneration[i] = Procreate(fitnessFunction, parentPopulation);
+            return ElitismSelection(parentPopulation, new Population(createGeneration));
+        }
+
+        private static IEnumerable<Tuple<int[], double>> EvolveForever(FSharpFunc<int[], double> fitnessFunction, Population initialPopulation, int childPopulationLimit)
+        {
+            var nextGeneration = initialPopulation;
+            while (true) 
+            {
+                var fitestIndividual = Fitest(nextGeneration);
+                yield return new Tuple<int[], double>(fitestIndividual.Individual.Genes, fitestIndividual.Fitness);
+                nextGeneration = EvolveOneGeneration(fitnessFunction, nextGeneration, childPopulationLimit);
+            }
+        }
+
+        public static IEnumerable<Tuple<int[],double>> Optimize(FSharpFunc<int[], double> fitnessFunction, int numberOfGenes, int numerOfIndividuals)
+        {
+            var initialPopulation = RandomIndividuals(fitnessFunction, numberOfGenes, numerOfIndividuals);
+            return EvolveForever(fitnessFunction, initialPopulation, numerOfIndividuals);
+        }
+    }
+}
---
+++ GeneticAlgorithm/Program.fs
@@ -0,0 +1,143 @@
+﻿module CAB402.FSharp.GeneticAlgorithm
+
+open RandomMonad
+
+// The genes of one individual within the population. Each gene is an integer.
+type Individual = int array
+
+// The genes of an individual together with an assessment of the fitness of that individual
+type ScoredIndividual = Individual * float 
+
+// a collection of scored individuals that make up a generation
+type Population = ScoredIndividual array
+
+// Find an individual within the population that has the highest fitness
+let fitest (population: Population) : ScoredIndividual = Array.maxBy (fun i -> snd i) population
+
+// Given a set of competeting individuals, return the winning individual (i.e. one with best fitness)
+let tournamentWinner (competitors: Population): Individual = fst (fitest competitors)
+
+// Randomly select an individual from a population by conducting a tournament.
+// A field of n = 2 competitors is first randomly generated and then the best individual within tht field is selected.
+// Each of individuals selected for the competition is selected independently, 
+// so it is possible that the same individual may be selected more than once.
+let tournamentSelect (population: Population) : Rand<Individual> =
+    rand {
+        let! n1 = chooseRandom population
+        let! n2 = chooseRandom population
+        return tournamentWinner [| n1; n2 |]
+    }
+
+// Combine the genes of parent1 and parent2 based on the given splitPoint 
+// The splitpoint will always be between 1 and length-1, where length is the length of both parent genes)
+// The genes in position 0..splitPoint will come directly from the corresponding genes of parent1
+// It is important the genes of the generated child is a legal permutation
+// i.e. it should include each of the integers between 0 and length-1 precisely once.
+// The order of the remaining genes of the child (those that were not inherited from parent1) are 
+// determined by the order that they occurred in parent2.
+// For example if parent1 = [0,3,5,4,2,1,6] and parent2 = [6,4,2,1,0,3,5] and the splitpoint is 4
+// then the first 4 genes come from parent1 [0,3,5,4] and the remaining genes [2,1,6] are ordered 
+// according to parent2 i.e. [6,2,1] because 6 comes before 2 and 2 comes before 1 in parent2.
+// So the child in this example will be [0,3,5,4,6,2,1]
+let crossAt (parent1: Individual) (parent2: Individual) (splitPoint: int): Individual =
+    let left, right = parent1 |> Array.splitAt splitPoint
+    let crossover = parent2 |> Array.filter (fun gene -> right |> Array.contains gene)
+    Array.append left crossover
+
+// Combine the genes of parent1 and parent2 at a randonly choosen splitpoint as per the above crossAt algorithm
+// The splitpoint is chosen so that both parents provide at least one gene to the child
+let cross (parent1: Individual) (parent2: Individual) : Rand<Individual> =
+    rand {
+        let! randomSplit = intRange 1 (parent1.Length-1)
+        return crossAt parent1 parent2 randomSplit
+    }
+
+// Return a mutated version of the original genes
+// the sequence of genes is split into 3 sections, a start, middle and end, based on the 2 provided indexes
+// (where 0 <= firstIndex < secondIndex < genes.length)
+// The start and end sections of the genes are left intact, while the genes in the middle section are reversed in order.
+// For example reverseMutateAt [0,3,5,4,2,1,6] 2 4 = [0,3,2,4,5,1,6]
+let reverseMutateAt (genes: Individual) (firstIndex: int) (secondIndex: int): Individual =
+    let startSection = genes.[..(firstIndex-1)]
+    let middleSection = genes.[firstIndex..secondIndex]
+    let endSection = genes.[(secondIndex+1)..]
+    Array.concat [| startSection; Array.rev middleSection; endSection |]
+
+// Perform a reverse mutation based on two randomly chosen split points
+// (such that 0 <= firstIndex < secondIndex < genes.length)
+let reverseMutate (chromosome: Individual): Rand<Individual> =
+    rand {
+        let! first = intRange 0 (chromosome.Length-1)
+        let! second = intRange (first+1) chromosome.Length
+        return reverseMutateAt chromosome first second
+    }
+
+let MutateProbability = 0.15
+
+// Perform a reverse mutation of the given genes with probability 0.15,
+// otherwise leave the sequence unaltered.
+let possiblyMutate (genes: Individual) : Rand<Individual> =
+    rand {
+        let! prob = withProbability MutateProbability
+        if prob then return! reverseMutate genes 
+        else return genes
+    }
+
+// Create a new population that consists of all of the children, plus the 10 best individuals from the previous generation.
+let elitismSelection (parents: Population) (children: Population) : Population =    
+    let bestParents = 
+        parents
+        |> Array.sortByDescending (fun p -> snd p)
+        |> Array.take 10
+    Array.append children bestParents
+
+// Create a scored individual by applying the fitness function to assess the fitness of the given genes.
+let score (fitnessFunction:Individual->float) (genes: Individual) : ScoredIndividual = (genes, fitnessFunction genes)
+
+// Randomly generate a population containing the specified number of individuals, each with the specified number of genes.
+let randomIndividuals (fitnessFunction:Individual->float) (numberGenes:int) (numberIndividuals:int) : Rand<Population> =
+    rand {
+        let! generateIndividuals = randArrayInit numberIndividuals (fun _ -> randomPermutation numberGenes)
+        return generateIndividuals |> Array.map (fun genes -> score fitnessFunction genes) // Generate and return ScoredIndividuals
+    }
+
+// Generate a child by first randomly choosing two parents using tournament selection,
+// cross their genes and then optionally mutate the resulting genes.
+// Note: individuals have no gender and each parent is chosen independently, 
+// so there is a small chance that the same individual may be choosen twice.
+let procreate fitnessFunction (population: Population) : Rand<ScoredIndividual> =
+    rand {
+        let! p1 = tournamentSelect population
+        let! p2 = tournamentSelect population
+        let! crossedGenes = cross p1 p2
+        let! probMutatedGenes = possiblyMutate crossedGenes
+        return score fitnessFunction probMutatedGenes
+    }
+
+// Create a new generation by creating the specified number of children through procreation and then 
+// applying elitism selection to create the population of the next generation
+let evolveOneGeneration fitnessFunction (parentPopulation: Population) (childPopulationLimit: int) : Rand<Population> =
+    rand {
+        let createChild = procreate fitnessFunction parentPopulation
+        let! createGeneration = randArrayInit childPopulationLimit (fun _ -> createChild)
+        return elitismSelection parentPopulation createGeneration
+    }
+
+// Starting with the specified initial population, evolve generation by generation (forever).
+// For each population, we determine the fitest individual from that generation and return an infinite sequence of these individuals.
+// Due to elitism selection, the fitest individual in each succcessive generation should be at least as good as the previous generation. 
+let evolveForever fitnessFunction (initialPopulation: Population) (childPopulationLimit: int): Rand<ScoredIndividual seq> =
+    initialPopulation
+    |> randSeqUnfold (fun currPopulation -> 
+        let fitestIndividual = fitest currPopulation
+        let nextGeneration = evolveOneGeneration fitnessFunction currPopulation childPopulationLimit
+        (fitestIndividual, nextGeneration))
+
+let Optimize fitnessFunction numberOfGenes numerOfIndividuals: ScoredIndividual seq =
+    let solutions =
+        rand {
+            let! initialPopulation = randomIndividuals fitnessFunction numberOfGenes numerOfIndividuals
+            return! evolveForever fitnessFunction initialPopulation numerOfIndividuals
+        }
+    let random = new System.Random()
+    RandomMonad.evaluateWith random solutions
